4c301249098f8853bc1c370aa6266e4602811c2f
KodGlobal <120708212+KodGlobal@users.noreply.github.com>
2025-04-01 03:55:01 +0300

    Update test_calculator_program.py

--- tests/test_calculator_program.py
+++ tests/test_calculator_program.py
@@ -12,9 +12,9 @@ def test_calculate_unknown_operation():
 
 '''
 Task. At the moment, there are 3 implemented unit-tests:
-The correctness of the calculator operation for addition, division, as well as the handling of the unknown operation
-Your taks is to add at least two tests for the following operations:
+We test the correctness of addition and division operations as well as the handling of the unknown operation
+Your task is to add at least two tests for the following operations:
 1. Subtraction
 2. Multiplication
-However, it would be great if you could come up with and add additional tests!
+However, it would be great if you could come up with and write some additional tests!
 '''
